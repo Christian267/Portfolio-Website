90bcdc70fc943d336edaefe4c6d4c7228cc89d92
Christian <christian.benitez@utexas.edu>
2021-11-07 17:32:05 -0600

    Finalized projects style, just need to add/adjust images

---
+++ src/components/projects/ProjectData.js
@@ -0,0 +1,52 @@
+import pathfindingImage1 from './images/pf1.png';
+import pathfindingImage2 from './images/pf1.gif';
+import chessAppImage1 from './images/chessapp1.png';
+import cityQolImage1 from './images/cityQol1.png';
+
+export const projectData = [];
+
+
+projectData.push({
+    title: 'Pathfinding Visualization',
+    description: 'Visualize multiple pathfinding algorithms using an interactive grid.',
+    buttonText: 'View Webpage',
+    image: pathfindingImage1,
+    hoverImage: pathfindingImage2,
+    link: 'https://christian267.github.io/Pathfinding-Visualization/'
+});
+
+projectData.push({
+    title: 'Chess Web Application',
+    description: 'Backend focused app which allows users to play chess together.',
+    buttonText: 'View Webpage',
+    image: chessAppImage1,
+    hoverImage: chessAppImage1,
+    link: 'http://chessapplication4-env.eba-u7exvhmc.us-east-2.elasticbeanstalk.com/'
+});
+
+projectData.push({
+    title: 'City Quality of Life Dashboard',
+    description: 'Full-stack application that displays the best suited city for user given their preferences on city quality of life metrics.',
+    buttonText: 'View Repository',
+    image: cityQolImage1,
+    hoverImage: cityQolImage1,
+    link: 'https://github.com/Christian267/City-QOL-Dashboard'
+});
+
+projectData.push({
+    title: 'Face Recognition App',
+    description: 'Trained a Keras FaceNet neural network to identify me from any given image.',
+    buttonText: 'View Repository',
+    image: pathfindingImage1,
+    hoverImage: pathfindingImage2,
+    link: 'https://github.com/Christian267/Face-Recognition-ML'
+});
+
+projectData.push({
+    title: 'Mouse and Keyboard Automation GUI',
+    description: 'Created a GUI to allow for easy creation and execution of scripts. These scripts allow for automation of repetitive mouse and keyboard actions.',
+    buttonText: 'View Repository',
+    image: pathfindingImage1,
+    hoverImage: pathfindingImage2,
+    link: 'https://github.com/Christian267/mouse-keyboard-automation'
+});
--- src/components/projects/ProjectTile.js
+++ src/components/projects/ProjectTile.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-export const ProjectTile = ({image, altImage, title, description, link}) => {
+export const ProjectTile = ({ title, description, buttonText, image, hoverImage, link}) => {
     return (
         <div className='project-tile'>
             <div className='text'>
                 <h3>{title}</h3>
                 <p>{description}</p>
-                <a href={link}><button>View Project</button></a>
+                <a href={link}><button>{buttonText}</button></a>
             </div>
             <div className='img-container'>
                 <img src={image} alt='Project' className='primary-image'/>
-                <img src={altImage} className='alt-image'/>
+                <img src={hoverImage} className='alt-image'/>
             </div>
         </div>
     );
