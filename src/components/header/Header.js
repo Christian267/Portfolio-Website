8bfde45f2c2ad7968010f28906f837002a114c5d
Christian <christian.benitez@utexas.edu>
2021-11-11 12:06:54 -0600

    finalized header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './Header.scss';
 import linus from './linus.jpg';
+import { FaGithub } from 'react-icons/fa';
+
 export const Header = () => {
     return (
         <div className='header' id='header'>
-            <div className='photo'>
-                <img src={linus} alt='Christian'/>
+            <div className='header-grid'>
+                <div className='photo'>
+                    <img src={linus} alt='Christian'/>
+                </div>
+                <div className='intro'><h1>Hello, my name is Christian Benitez.</h1></div>
+                <div className='description'><p>I am an aspiring software engineer, specializing in backend web development.</p></div>
             </div>
-            <div className='intro'><h1>Hello, my name is Christian Benitez.</h1></div>
-            <div className='description'><p>I am an aspiring software engineer, specializing in backend web development.</p></div>
-            <div className='links'>links</div>
+            <span><h5><a href='https://github.com/christian267'><FaGithub className='icon'/></a></h5></span>
         </div>
     );
 };
