193212d3f234726fcf3a9963bad0a8f607b0a955
Christian <christian.benitez@utexas.edu>
2021-11-27 17:03:57 -0600

    Added photo of self into header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Header.scss';
-import linus from './linus.jpg';
+import portrait from './portrait3.JPG';
 import { FaGithub } from 'react-icons/fa';
 
 export const Header = () => {
@@ -8,10 +8,10 @@ export const Header = () => {
         <div className='header' id='header'>
             <div className='header-grid'>
                 <div className='photo'>
-                    <img src={linus} alt='Christian'/>
+                    <img src={portrait} alt='Christian'/>
                 </div>
                 <div className='intro'><h1>Hello, my name is Christian Benitez.</h1></div>
-                <div className='description'><p>I am an aspiring software engineer, specializing in backend web development.</p></div>
+                <div className='description'><p>I am an aspiring software engineer, focusing on backend web development.</p></div>
             </div>
             <span><h5>Christian.benitez2797@gmail.com<a href='https://github.com/christian267'><FaGithub className='icon'/></a></h5></span>
         </div>
