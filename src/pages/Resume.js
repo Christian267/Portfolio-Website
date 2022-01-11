2d095b10ed94129a0c17ac9ab6a6efe0bc650d5f
Christian <christian.benitez@utexas.edu>
2022-01-11 13:09:58 -0600

    made resume page responsive to mobile

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 
 export const Resume = () => {
+    const [matches, setMatches] = useState(
+        window.matchMedia("(min-width: 768px)").matches
+    )
     const [numPages, setNumPages] = useState(null);
     const [pageNumber, setPageNumber] = useState(1);
 
@@ -9,6 +12,11 @@ export const Resume = () => {
         setNumPages(numPages);
     }
 
+    useEffect(() => {
+        window.matchMedia("(min-width: 768px)")
+        .addEventListener('change', e => setMatches( e.matches));
+    }, []);
+    
     return (
         <div className='resume-container'>
             <Document
@@ -18,7 +26,7 @@ export const Resume = () => {
                 <Page 
                     className='page'
                     pageNumber={pageNumber}
-                      height={1000}
+                      height={matches ? 1000: 635}
                 />
             </Document>
         </div>
