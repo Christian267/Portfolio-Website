2fb5e0f0ee96344ca815a242297532a86dae84e0
Christian <christian.benitez@utexas.edu>
2022-01-11 12:45:14 -0600

    Styled Resume and created route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import './App.scss';
 import { HomePage } from './pages/HomePage';
 import { Resume } from './pages/Resume.js';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
         <Router>
-          <Switch>
-            <Route path="/resume">
-              <Resume />
-            </Route>
-            <Route path="/">
-              <HomePage />
-            </Route>
-          </Switch>
+          <Routes>
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/" element={<HomePage />} />
+          </Routes>
         </Router>
     </div>
   );
